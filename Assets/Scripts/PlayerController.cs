e3727923773825d967cf2c03cd637142a0510d23
b.pustovyi <b.pustovyi@e-tender.biz>
2017-04-26 17:33:24 +0300

    Trying to move robot

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,33 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Rigidbody rb;
+    private int i = 0;
+    private int j = 0;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Invoke("Example", 0.3f);
     }
 
+    void Example()
+    {
+        transform.position = new Vector3(i-9, 0.5f, j-9);
+        j += (i + 1) / 19;
+        i = (i + 1) % 19;
+        Invoke("Example", 0.3f);
+    }
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce(movement * speed);
+        print(Input.anyKeyDown.ToString());
+        Vector3 movement = new Vector3(5, 0.0f, 5);
+        if (Input.anyKeyDown)
+        {
+            transform.position = new Vector3(i, 0.5f, i);
+            i++;
+        }
+        //transform.position = new Vector3(rb.position.x+moveHorizontal, rb.position.y, rb.position.z+moveVertical);
     }
 }
