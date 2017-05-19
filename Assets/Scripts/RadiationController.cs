216317c8f69ac0d0c92e0705c6acfc607f0300e4
b.pustovyi <b.pustovyi@e-tender.biz>
2017-05-19 18:36:34 +0300

    Alpha-Beta-Gamma radiation added

---
+++ Assets/Scripts/RadiationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadiationController : MonoBehaviour {
+
+    public float speed;
+    public int doze;
+    public float maxLenght;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale += new Vector3(0.01f, 0.01f, 0.01f)*speed;
+        if (transform.localScale.x >= maxLenght)
+        {
+            transform.localScale = new Vector3(0, 0, 0);
+        }
+		
+	}
+}
